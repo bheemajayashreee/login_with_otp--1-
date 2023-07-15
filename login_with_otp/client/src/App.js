f512f39469f53afc3990ac5d2ba10323744810ba
Jayashree <jayashreebheema@gmail.com>
2023-07-15 13:09:38 +0530

    first commit

---
+++ login_with_otp/client/src/App.js
@@ -0,0 +1,37 @@
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Otp from "./pages/Otp";
+import Error from "./pages/Error";
+import Headers from "./components/Headers";
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Regnum from "./pages/Regnum";
+import Predict from "./pages/Predict";
+import Past from "./pages/Past";
+function App() {
+  return (
+    <>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user/otp" element={<Otp />} />
+        <Route path="/regnum" element={<Regnum />} />
+        <Route path="/predict" element={<Predict />} />
+        <Route path="/user/past" element={<Past />} />
+        
+
+        
+
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
